6d7525a397195783816f2aeba879175ee1c52a30
mirnatul <www.mirnatul@gmail.com>
2025-05-13 15:23:32 +0600

    installing pandas and push other code

---
+++ test_pandas.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+data = np.array([["", "Col1", "Col2"], ["Row1", 1, 2], ["Row2", 3, 4]])
+print(data)
+
+import pandas as pd
+
+print(pd.DataFrame(data=data[1:, 1:], index=data[1:, 0], columns=data[0, 1:]))
